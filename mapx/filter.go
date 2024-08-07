5facc282feed78a2625673d930751b68a5e00d60
jeseeqing <jeseeqing@gmail.com>
2024-08-07 21:31:04 +0800

    add FilterByKeys in mapx package

--- mapx/filter.go
+++ mapx/filter.go
@@ -13,3 +13,13 @@ func FilterInMapString[v any](m map[string]v, fn func(string, v) bool) map[strin
 	}
 	return result
 }
+
+func FilterByKeys[v any](m map[string]v, keys []string) map[string]v {
+	result := make(map[string]v)
+	for _, key := range keys {
+		if value, exists := m[key]; exists {
+			result[key] = value
+		}
+	}
+	return result
+}
