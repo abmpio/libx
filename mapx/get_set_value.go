8b891d7c09007f5e8061dd828101e29e559759ec
jeseeqing <jeseeqing@gmail.com>
2024-08-12 15:58:16 +0800

    add GetValueAs,GetValueAsXXX,SetValue func in mapx package

---
+++ mapx/get_set_value.go
@@ -0,0 +1,52 @@
+package mapx
+
+import "time"
+
+func GetAsInt64Ptr(m map[string]interface{}, key string) *int64 {
+	v, _ := GetValueAs[*int64](m, key)
+	return v
+}
+
+func GetAsBoolPtr(m map[string]interface{}, key string) *bool {
+	v, _ := GetValueAs[*bool](m, key)
+	return v
+}
+
+func GetAsTimePtr(m map[string]interface{}, key string) *time.Time {
+	v, _ := GetValueAs[*time.Time](m, key)
+	return v
+}
+
+func GetAsString(m map[string]interface{}, key string) string {
+	v, _ := GetValueAs[string](m, key)
+	return v
+}
+
+func SetValue(m map[string]interface{}, key string, v interface{}, removeIfIsNil bool) {
+	if m == nil {
+		return
+	}
+	if removeIfIsNil && v == nil {
+		delete(m, key)
+	} else {
+		m[key] = v
+	}
+}
+
+// get a key value from map[string]interface{},
+// T: try convert value to T, if cannot convert,return zero T value,and false
+func GetValueAs[T any](m map[string]interface{}, key string) (T, bool) {
+	var value T
+	if len(m) <= 0 {
+		return value, false
+	}
+	v, ok := m[key]
+	if !ok {
+		return value, false
+	}
+	convertedV, ok := v.(T)
+	if !ok {
+		return value, false
+	}
+	return convertedV, true
+}
